66b7d87d4a7c659f096dd38816d6049e860a5ee5
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-30 14:04:57 -0500

    Initial Commit

---
+++ src/RandomExercises/Recursion.java
@@ -0,0 +1,27 @@
+package RandomExercises;
+
+public class Recursion {
+
+    // TODO: use recursion to print out a given number up through 100
+//     public static int countTo100(int num) {
+//
+//
+//     }
+
+
+    // TODO: use recursion to add all numbers up from 1 to a given number
+    // public static int addNums(int num) {
+    // }
+
+    public static void main(String args[]){
+
+
+
+
+
+
+
+
+
+    }
+}
