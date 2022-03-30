b78a5cac139882ce51892e1d272e796daf8fb41c
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-29 20:46:39 -0500

    Created a program that takes an input for hours and wages. The results will be in grossPay and then displayed with the system out print.

--- src/GrossPayCalculator/GrossPayCalculator.java
+++ src/GrossPayCalculator/GrossPayCalculator.java
@@ -1,5 +1,7 @@
 package GrossPayCalculator;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
     public static void main(String[] args) {
 
@@ -12,6 +14,22 @@ public class GrossPayCalculator {
         double grossPay = hours * payRate;
         //-- 4. Display results
         System.out.println("Gross pay: " + grossPay);
+
+        System.out.println("----------------------");
+
+        //----- User Input Exercise
+        Scanner scanner = new Scanner(System.in);               // Mandatory for you to take in inputs
+        //-- 1. Get the number of hours worked
+        System.out.println("How many hours did you work?");
+        int hoursWorkedThisWeek = scanner.nextInt();            // Takes In Answer
+        //-- 2. Get the hourly pay rate
+        System.out.println("What is your pay rate?");
+        double yourPayRate = scanner.nextDouble();
+        scanner.close();        // Closes the scanner
+        //-- 3. Multiply hours and pay rate
+        double grossPay2 = hoursWorkedThisWeek * yourPayRate;
+        //-- 4. Display results
+        System.out.println("Gross pay: " + grossPay2);
 
     }
 }
