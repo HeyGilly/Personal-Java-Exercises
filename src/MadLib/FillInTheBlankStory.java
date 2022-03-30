7ccf5553a422b45881c3387217dfb69d1e72eb95
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-29 21:06:22 -0500

    Created a program that takes an input and places them in a premade statement filled with the variables that took the input from the user.

--- src/MadLib/FillInTheBlankStory.java
+++ src/MadLib/FillInTheBlankStory.java
@@ -1,8 +1,26 @@
 package MadLib;
 
+import java.util.Scanner;
+
 public class FillInTheBlankStory {
 
+        public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.println("Let's Create a story!");
+            System.out.println("-------------------------");
+            System.out.println("Enter an adjective");
+            String adjective = scanner.next();
 
+            System.out.println("Enter a season of the year");
+            String season = scanner.next();
 
+            System.out.println("Enter a whole number");
+            int num = scanner.nextInt();
+
+            scanner.close();
+
+            System.out.println("On a(n) "+adjective+ " "+season+" day, I drink a minimum of "+
+                    num + " cups of coffee.");
+        }
 }
