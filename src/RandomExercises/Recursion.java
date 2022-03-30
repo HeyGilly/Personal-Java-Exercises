fac26b0b1e521bad626b0d80f9d8865450946d51
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-30 14:46:26 -0500

    used recursion to print out a given number up through 100 and to add all numbers up from 1 to a given number

--- src/RandomExercises/Recursion.java
+++ src/RandomExercises/Recursion.java
@@ -3,25 +3,29 @@ package RandomExercises;
 public class Recursion {
 
     // TODO: use recursion to print out a given number up through 100
-//     public static int countTo100(int num) {
-//
-//
-//     }
-
+         public static void countTo100(int num) {
+             if (num >= 101) {
+                 System.out.print("All done!");
+                 return;
+             }
+             System.out.print(num + ", ");
+             countTo100(num + 1);
+         }
 
     // TODO: use recursion to add all numbers up from 1 to a given number
-    // public static int addNums(int num) {
-    // }
+     public static void addNums(int num) {
+         if (num >= 101) {
+             System.out.print("All done!");
+             return;
+         }
+         System.out.print(num + ", ");
+         addNums(num + 1);
+     }
 
-    public static void main(String args[]){
-
-
-
-
-
-
-
-
-
+    public static void main(String[] args) {
+             System.out.println("\n------------------------\n");
+        countTo100(90);
+             System.out.println("\n------------------------\n");
+        addNums(25);
     }
 }
