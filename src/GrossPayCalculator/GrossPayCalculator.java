bd2689ab9729cf2e85c0ac4a94b276502f7d85ed
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-29 20:37:15 -0500

    Created a hard coded program that takes a full 40-hour work week with a fixed pay. The results will be in grossPay and then displayed with the system out print

--- src/GrossPayCalculator/GrossPayCalculator.java
+++ src/GrossPayCalculator/GrossPayCalculator.java
@@ -3,6 +3,15 @@ package GrossPayCalculator;
 public class GrossPayCalculator {
     public static void main(String[] args) {
 
+        //----- Hard Coded Exercise
+        //-- 1. Get the number of hours worked
+        int hours = 40;
+        //-- 2. Get the hourly pay rate
+        double payRate = 25.50;
+        //-- 3. Multiply hours and pay rate
+        double grossPay = hours * payRate;
+        //-- 4. Display results
+        System.out.println("Gross pay: " + grossPay);
 
     }
 }
