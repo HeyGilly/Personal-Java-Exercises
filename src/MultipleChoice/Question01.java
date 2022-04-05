3434cc6f385e8b8c210d1aae8dcf2452beb241de
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-05 11:50:11 -0500

    Created a program that asked the user a question with choices pre-made. The while-loop will run once and the parameter is if the answer is true. It will ask the question and print the choices. The user gives their input. If the correct answer equals the user input to lowercase, the user gets congrats. If the user input is not equal to the correct answer, it tells the user and asks the question again until it is correct.

--- src/MultipleChoice/Question01.java
+++ src/MultipleChoice/Question01.java
@@ -1,7 +1,41 @@
 package MultipleChoice;
 
+import java.util.Scanner;
 
 public class Question01 {
+
+    public static void main(String[] args) {
+        String question = "Which allows the storage of a null key and null values?\n";
+        String choiceOne = "hashtable";
+        String choiceTwo = "hashmap";
+        String choiceThree = "neither";
+        Boolean answerIs = true;
+
+        String correctAnswer = choiceTwo;
+
+        while (answerIs) {
+            System.out.println(question);
+            System.out.println("Choose one of the following: " + choiceOne + ", " + choiceTwo + ", or " + choiceThree + "? ");
+
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.next();
+
+
+            if (correctAnswer.equals(userInput.toLowerCase())) {
+                System.out.println("Congrats! you're right!");
+                answerIs = false;
+            }else{
+                System.out.println("----------------------------");
+                System.out.println("Sorry that is incorrect, guess again");
+                System.out.println("----------------------------");
+
+            }
+
+        }
+        System.out.println("-----------Next Question-----------------");
+
+    }
+
 
 
 
