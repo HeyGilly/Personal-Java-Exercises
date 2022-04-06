437e2fe8ebb7ed2d9777f9b187f164d91907b68f
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-06 14:37:17 -0500

    Created a program that will create a new user profile and then run an increment to their graduating year. finally, prints out the new year that student will graduate.

--- src/StudentProfile/Main.java
+++ src/StudentProfile/Main.java
@@ -1,4 +1,14 @@
 package StudentProfile;
 
+
 public class Main {
+
+
+    public static void main(String[] args) {
+    StudentProfile studentOne = new StudentProfile("Mark", "Locke", "Science", 3.5, 2023);
+
+    studentOne.incrementExpectedGraduationYear();       // THis add one to the year
+    System.out.println(studentOne.expectedYearToGraduate);
+
+    }
 }
--- src/StudentProfile/StudentProfile.java
+++ src/StudentProfile/StudentProfile.java
@@ -1,5 +1,25 @@
 package StudentProfile;
 
 public class StudentProfile {
+    String fName;
+    String lName;
+    String declaredMajor;
+    double gpa;
+    int expectedYearToGraduate;
+
+    public StudentProfile(String fName, String lName, String declaredMajor, double gpa, int expectedYearToGraduate){
+        this.fName = fName;
+        this.lName = lName;
+        this.declaredMajor = declaredMajor;
+        this.gpa = gpa;
+        this.expectedYearToGraduate = expectedYearToGraduate;
+    }
+
+    public void incrementExpectedGraduationYear(){
+        this.expectedYearToGraduate = this.expectedYearToGraduate + 1;
+
+
+    }
+
 }
 
