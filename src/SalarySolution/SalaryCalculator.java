87eefa3169d54b103b121ccb8bdabe1a5ecf50c4
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-05 19:47:15 -0500

    created a calculator that takes in the hours worked, hourly pay, and vocation days. if negative the program will return a -1.

--- src/SalarySolution/SalaryCalculator.java
+++ src/SalarySolution/SalaryCalculator.java
@@ -1,4 +1,28 @@
 package SalarySolution;
 
 public class SalaryCalculator {
+
+    public static double negativePositive(double hoursPerWeek, double amountPerHour, int vocationDays){
+        if (hoursPerWeek < 0){
+            return -1;
+        }
+        if(amountPerHour < 0){
+            return -1;
+        }
+        double weeklyPaycheck = hoursPerWeek * amountPerHour;
+        double unpaidTime = vocationDays * amountPerHour * 8;
+        return (weeklyPaycheck * 52) - unpaidTime;
+    };
+
+    public static double salaryCal(double hoursPerWeek, double amountPerHour, int vocationDays) {
+       return negativePositive(hoursPerWeek, amountPerHour, vocationDays);
+    };
+
+    public static void main(String[] args) {
+        double salary = salaryCal(-5, 15, 8);
+        System.out.println(salary);
+    }
+
+
+
 }
