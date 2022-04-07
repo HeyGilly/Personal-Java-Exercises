ef1dd395e154c0497a2cdb1dfb72bd1066e65322
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-04-07 15:32:15 -0500

    Created two new objects with different parameters. First print shows the pay before the raise, the method runs then showcases the new salary for both new objects.

--- src/GrossPayCalculator/Employee.java
+++ src/GrossPayCalculator/Employee.java
@@ -1,6 +1,20 @@
 package GrossPayCalculator;
 
 public class Employee {
+    String fullName;
+    String location;
+    double salary;
+    int age;
 
+    Employee(String fullName, String location, double salary, int age){
+        this.fullName = fullName;
+        this.location = location;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    void raiseSalary(){
+        this.salary = this.salary * 1.2;
+    }
 
 }
--- src/GrossPayCalculator/Main.java
+++ src/GrossPayCalculator/Main.java
@@ -1,4 +1,25 @@
 package GrossPayCalculator;
 
 public class Main {
+
+    public static void main(String[] args) {
+        //Created new objects by calling the class with custom instance
+        Employee bob = new Employee("Bobby Smith", "Los Angeles", 70000, 35);
+        Employee lucy = new Employee("Lucy layla", "Toronto", 78000, 32);
+
+        //Before pay raise
+        System.out.println(bob.salary);
+        System.out.println(lucy.salary);
+
+        //Calling the method to perform
+        //the task of raise Salary.
+        bob.raiseSalary();
+        lucy.raiseSalary();
+
+        //After Pay raise
+        System.out.println(bob.salary);
+        System.out.println(lucy.salary);
+    }
+
+
 }
